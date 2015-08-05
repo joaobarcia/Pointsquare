ad0bffab9147a99f1b5d857ece2f2d66ba8ce536
joaobarcia <joaobarcia@gmail.com>
2015-08-05 16:03:59 +0100

    Fixed title

--- both/router/meta.js
+++ both/router/meta.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
     Meta.config({
         options: {
             // Meteor.settings[Meteor.settings.environment].public.meta.title
-            title: 'Diskorse',
+            title: 'Pointsquare',
             suffix: 'Suffix'
         }
     });
