7f70e241d42766301e39c34abe1846d52cf115f7
joaobarcia <joaobarcia@gmail.com>
2015-09-09 14:33:26 +0100

    awesome analytics

---
+++ client/analytics/config.js
@@ -0,0 +1,7 @@
+/*// MAIN ANALYTICS SCRIPTS FOR HOTJAR AND WOOPRA ON INDEX.HTML HEAD
+Accounts.onLogin(function(user) {
+     Place this on a template where a customer initially is identified
+   or after authentication. (Important: Update these values) 
+
+});
+*/
