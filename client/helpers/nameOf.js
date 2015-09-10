f7608f97455af13a12a42d4396ce62e2943b25a0
joaobarcia <joaobarcia@gmail.com>
2015-09-10 01:14:49 +0100

    upgrades to dashboard, restructured code

--- client/helpers/nameOf.js
+++ client/helpers/nameOf.js
@@ -1,6 +1,7 @@
 // ONLY SHOWS 
 
 Template.registerHelper('nameOf', function(authorsRid, options) {
+    console.log("oupu")
     return people.findOne({
         'rid': authorsRid[0].rid
     }).name;
