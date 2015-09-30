26966c204cf6ab4146dfa3395ff1f3444b20b6e3
Xenophyte <david.seixas@gmail.com>
2015-09-30 17:28:06 +0100

    consegue aprender mesmo quando o pré-cálculo ainda não acabou

--- both/router/routes.js
+++ both/router/routes.js
@@ -51,10 +51,35 @@ Router.route('/unit/:_escrid', {
         var unit = decodeURIComponent(this.params._escrid);
         var user = Session.get('currentUserRID');
         Session.set("temp","precomputing");
-        Meteor.call('precompute',unit,user, function(error,result){
+        Meteor.call('precompute',unit, function(error,result){
             if (result.statusCode >= 200 && result.statusCode < 300) {
                 Session.set("temp","ready");
                 console.log('ready to learn');
+                // if( Session.get('callStatus') == 'doingExercise' ){
+                //     return result;
+                // }
+                if( Session.get('callStatus') == 'learning' ){
+                    Meteor.call("succeed",unit, function(error, result) {
+                        if (result.statusCode >= 200 && result.statusCode < 300) {
+                            Session.set('callStatus', 'learned');
+                        }
+                        var newStuff = result.data.result[0]['value'] //["content"];//["result"];//[0]["value"];
+                        Session.set('newConcepts', newStuff[1]);
+                        Session.set('newUnits', newStuff[0]);
+                    });
+                    Meteor.call("incrementViews", unit);
+                }
+                else if( Session.get('callStatus') == 'unlearning' ){
+                    Meteor.call("fail",unit, function(error, result) {
+                        if (result.statusCode >= 200 && result.statusCode < 300) {
+                            Session.set('callStatus', 'unlearned');
+                        }
+                        var newStuff = result.data.result[0]['value'] //["content"];//["result"];//[0]["value"];
+                        Session.set('lostConcepts', newStuff[3]);
+                        Session.set('lostUnits', newStuff[2]);
+                    });
+                    Meteor.call("incrementViews", unit);
+                };
             }
         });
         this.next();
--- client/templates/unitPage/unitPage.js
+++ client/templates/unitPage/unitPage.js
@@ -16,15 +16,29 @@ Template.unitPage.events({
     'click #understood': function(event) {
         event.preventDefault();
         Session.set('callStatus', 'learning');
-        Meteor.call("succeed",Blaze.getData()["rid"], function(error, result) {
-            console.log(result.statusCode);
-            if (result.statusCode >= 200 && result.statusCode < 300) {
-                Session.set('callStatus', 'learned');
-            }
-            var newStuff = result.data.result[0]['value'] //["content"];//["result"];//[0]["value"];
-            Session.set('newConcepts', newStuff[1]);
-            Session.set('newUnits', newStuff[0]);
-        });
+        if( Session.get("temp")=="ready" ){
+            Meteor.call("succeed",Blaze.getData()["rid"], function(error, result) {
+                console.log(result.statusCode);
+                if (result.statusCode >= 200 && result.statusCode < 300) {
+                    Session.set('callStatus', 'learned');
+                }
+                var newStuff = result.data.result[0]['value'] //["content"];//["result"];//[0]["value"];
+                Session.set('newConcepts', newStuff[1]);
+                Session.set('newUnits', newStuff[0]);
+            });
+        }
+        else if( Session.get("temp")=="precomputing" ){
+            console.log("nothing");
+        }
+        // Meteor.call("succeed",Blaze.getData()["rid"], function(error, result) {
+        //     console.log(result.statusCode);
+        //     if (result.statusCode >= 200 && result.statusCode < 300) {
+        //         Session.set('callStatus', 'learned');
+        //     }
+        //     var newStuff = result.data.result[0]['value'] //["content"];//["result"];//[0]["value"];
+        //     Session.set('newConcepts', newStuff[1]);
+        //     Session.set('newUnits', newStuff[0]);
+        // });
         // Meteor.call("learn", "1", Blaze.getData()["rid"], function(error, result) {
         //     console.log(result.statusCode);
         //     if (result.statusCode >= 200 && result.statusCode < 300) {
@@ -41,15 +55,29 @@ Template.unitPage.events({
     'click #notUnderstood': function(event) {
         event.preventDefault();
         Session.set('callStatus', 'unlearning');
-        Meteor.call("fail",Blaze.getData()["rid"], function(error, result) {
-            console.log(result.statusCode);
-            if (result.statusCode >= 200 && result.statusCode < 300) {
-                Session.set('callStatus', 'unlearned');
-            }
-            var newStuff = result.data.result[0]['value'];
-            Session.set('lostConcepts', newStuff[3]);
-            Session.set('lostUnits', newStuff[2]);
-        });
+        if( Session.get("temp")=="ready" ){
+            Meteor.call("fail",Blaze.getData()["rid"], function(error, result) {
+                console.log(result.statusCode);
+                if (result.statusCode >= 200 && result.statusCode < 300) {
+                    Session.set('callStatus', 'unlearned');
+                }
+                var newStuff = result.data.result[0]['value'];
+                Session.set('lostConcepts', newStuff[3]);
+                Session.set('lostUnits', newStuff[2]);
+            });
+        }
+        else if( Session.get("temp")=="precomputing" ){
+            console.log("nothing");
+        }
+        // Meteor.call("fail",Blaze.getData()["rid"], function(error, result) {
+        //     console.log(result.statusCode);
+        //     if (result.statusCode >= 200 && result.statusCode < 300) {
+        //         Session.set('callStatus', 'unlearned');
+        //     }
+        //     var newStuff = result.data.result[0]['value'];
+        //     Session.set('lostConcepts', newStuff[3]);
+        //     Session.set('lostUnits', newStuff[2]);
+        // });
         // Meteor.call("learn", "0", Blaze.getData()["rid"], function(error, result) {
         //     if (result.statusCode >= 200 && result.statusCode < 300) {
         //         Session.set('callStatus', 'unlearned');
