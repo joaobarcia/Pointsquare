543421cf5a872f71242f3dc246009be93e02bfc6
joaobarcia <joaobarcia@gmail.com>
2015-10-10 14:22:27 +0100

    home guides, css fixes, create page not necessary anymore, unit state to color detects result errors

--- client/helpers/mapUnitStateToColor.js
+++ client/helpers/mapUnitStateToColor.js
@@ -1,9 +1,10 @@
 Template.registerHelper('mapUnitStateToColor', function(stateValue) {
     if (!Meteor.userId()) {
-        return "cyan";
+        return "cyan"; // when user is not logged show neutral color cyan
     } else {
-        if (stateValue < 0.2) return "red lighten-2";
-        else if (stateValue < 0.8) return "orange lighten-2";
-        else return "green"
+        if (stateValue >= 0 && stateValue < 0.2) return "red lighten-2"; // if concept state is low display red book
+        else if (stateValue < 0.8) return "orange lighten-2"; // if concept state is intermediate display yellow book
+        else if (stateValue <= 1) "green"; // if concept state is high display green book
+        else return "grey"; // any other value (state error) display grey book
     }
 });
--- client/templates/globalLayout/_header.js
+++ client/templates/globalLayout/_header.js
@@ -1,13 +1,11 @@
-/*Template._header.rendered = function() {
-    $(document).ready(function() {
-        this.$('.tooltipped').tooltip({
-            delay: 20
-        });
-        this.$('.modal-trigger').leanModal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 300, // Transition in duration
-            out_duration: 200, // Transition out duration
-        });
-    });
-};*/
+Template._headerUserIsLogged.rendered = function() {
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        //constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        //gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        //alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    })
+};
