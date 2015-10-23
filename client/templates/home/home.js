a0c2a1f63fcafaca23b9b562a648d9dd7006d460
joaobarcia <joaobarcia@gmail.com>
2015-10-23 17:09:56 +0100

    feedback button working on home page fix

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -5,3 +5,20 @@ Template.home.rendered = function() {
         });
     });
 };
+
+Template.currentUserHome.rendered = function() {
+    $(document).ready(function() {
+        $('.collapsible').collapsible({
+            accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+        });
+        $('.tooltipped').tooltip({
+            delay: 20
+        });
+    });
+};
