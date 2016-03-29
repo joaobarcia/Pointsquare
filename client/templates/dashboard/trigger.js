699d974b40aed03f454e4152e824ffa924e3ad8c
joaobarcia <joaobarcia@gmail.com>
2016-03-29 19:35:27 +0100

    reset user knowledge no dashboard, varios acertos

--- client/templates/dashboard/trigger.js
+++ client/templates/dashboard/trigger.js
@@ -6,3 +6,11 @@ Template.dashboard.onCreated(function() {
         self.subscribe('goals');
     });
 });
+
+Template.dashboard.events({
+  'click #resetUserKnowledge': function(event) {
+    event.preventDefault();
+    var userId = Meteor.userId();
+    Meteor.call('resetUser', userId);
+  }
+});
