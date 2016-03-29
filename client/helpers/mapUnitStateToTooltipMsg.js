dc9a3b66e5ae33ee4acaad5b51324bbd9d06ede2
joaobarcia <joaobarcia@gmail.com>
2016-03-29 21:57:39 +0100

    unit card tooltips, unitpage sem links para pag conceito, david pode fazer upload

---
+++ client/helpers/mapUnitStateToTooltipMsg.js
@@ -0,0 +1,10 @@
+Template.registerHelper('mapUnitStateToTooltipMsg', function(stateValue) {
+    if (!Meteor.userId()) {
+        return "Login to see if this unit is suitable for you"; // when user is not logged
+    } else {
+        if (stateValue >= 0 && stateValue < 0.2) return "Unit is might be too hard for you"; // if concept state is low
+        else if (stateValue < 0.8) return "Unit might be right for you"; // if concept state is intermediate
+        else if (stateValue <= 1) return "Unit is at the right level for you"; // if concept state is high
+        else return "grey"; // any other value (state error) display
+    }
+});
