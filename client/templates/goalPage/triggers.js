93ac03f42ae4cc427dba162a3566da64ef48affb
joaobarcia <joaobarcia@gmail.com>
2016-04-13 07:59:57 +0200

    esperar no goalPage até mostrar info + update meteor

--- client/templates/goalPage/triggers.js
+++ client/templates/goalPage/triggers.js
@@ -1,31 +1,47 @@
 Template.goalPage.onCreated(function() {
-  var self = this;
-  self.autorun(function() {
-    self.subscribe('nodes');
-    self.subscribe('personal');
-    self.subscribe('goals');
-  });
+    Session.set("isLoading", true);
+    var self = this;
+    self.autorun(function() {
+        self.subscribe('nodes');
+        self.subscribe('personal');
+        self.subscribe('goals');
+    });
+    // if a goal exists
+    if (typeof Goals.findOne({
+            user: Meteor.userId()
+        }) !== "undefined") {
+
+        var goal = Goals.findOne({
+            user: Meteor.userId()
+        });
+        // re-calculate it
+        Meteor.call("setGoal", goal.node, Meteor.userId(), function(e, r) {
+            Session.set("isLoading", false);
+        });
+    } else {
+        Session.set("isLoading", false);
+    }
 });
 
 Template.goalPage.events({
-  'click #removeGoal': function(event) {
-    event.preventDefault();
-    var userId = Meteor.userId();
-    var goalId = Goals.findOne({
-      user: Meteor.userId()
-    }).node;
-    Meteor.call('removeGoal', goalId, userId);
-  }
+    'click #removeGoal': function(event) {
+        event.preventDefault();
+        var userId = Meteor.userId();
+        var goalId = Goals.findOne({
+            user: Meteor.userId()
+        }).node;
+        Meteor.call('removeGoal', goalId, userId);
+    }
 });
 
 Template.goalPage.onDestroyed(function() {
-  if (Session.equals('removeGoalOnDestroyGoalPage', true)) {
-    Session.set('removeGoalOnDestroyGoalPage', false);
-    var userId = Meteor.userId();
-    var goalId = Goals.findOne({
-      user: Meteor.userId()
-    }).node;
-    console.log('removed goal!');
-    Meteor.call('removeGoal', goalId, userId);
-  }
+    if (Session.equals('removeGoalOnDestroyGoalPage', true)) {
+        Session.set('removeGoalOnDestroyGoalPage', false);
+        var userId = Meteor.userId();
+        var goalId = Goals.findOne({
+            user: Meteor.userId()
+        }).node;
+        console.log('removed goal!');
+        Meteor.call('removeGoal', goalId, userId);
+    }
 });
--- server/kadira/kadira.js
+++ server/kadira/kadira.js
@@ -1,3 +1,3 @@
-Meteor.startup(function() {
-  Kadira.connect('TPT4wtW39iNLndDjx', '40694a0f-8cf5-4e03-ac57-432dfe191369'); 
-});
+// Meteor.startup(function() {
+//   Kadira.connect('TPT4wtW39iNLndDjx', '40694a0f-8cf5-4e03-ac57-432dfe191369');
+// });
