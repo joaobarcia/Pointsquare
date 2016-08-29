9102d317c0f3a4b2f2f16dbea195880929bcca56
Xenophyte <david.seixas@gmail.com>
2016-08-29 13:09:13 +0100

    limpeza de código

--- client/templates/createExam/triggers.js
+++ client/templates/createExam/triggers.js
@@ -1,15 +1,6 @@
 AutoForm.hooks({
     createExam: {
         onSubmit: function(doc) {
-            /*var exercisesMappedAsObject = {};
-            if (doc.exercises != null) {
-                for (var i = 0; i < doc.exercises.length; i += 1) {
-                    exercisesMappedAsObject[doc.exercises[i]] = true;
-                }
-                doc.exercises = exercisesMappedAsObject;
-            };
-            delete doc.exercises;
-            console.log(exercisesMappedAsObject);*/
 
             var examDefinitions = {}
             examDefinitions.type = 'exam';
