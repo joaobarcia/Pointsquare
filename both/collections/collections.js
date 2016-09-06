f3b81b32591c60563507ffc766f52e2ecb6acb54
joaobarcia <joaobarcia@gmail.com>
2016-09-06 19:15:18 +0100

    apagar colecoes desnecessarias + not found page + conceptEdit remains in conceptEdit after submit + show concepts user knows in dashboard

--- both/collections/collections.js
+++ both/collections/collections.js
@@ -1,6 +1,5 @@
 Nodes = new Mongo.Collection("nodes");
 Personal = new Mongo.Collection("personal");
-Goals = new Mongo.Collection("goals");
 
 // Collections for comments package (currently disabled)
 //comments = new Mongo.Collection("comments");
