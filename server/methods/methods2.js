78299a1d06ecc7fbe4d175df7911b7b662f34e16
David de Sousa Seixas <david.seixas@gmail.com>
2016-09-08 16:16:56 +0100

    desbugamento setGoal e userKnows

--- server/methods/methods2.js
+++ server/methods/methods2.js
@@ -1624,14 +1624,6 @@ Meteor.methods({
   },
 
   setGoal: function(node_id, user_id, not_in = {}){
-      /*var contains = Nodes.findOne(exam_id).contains;
-      var exercises = {};
-      var id;
-      for(var i in contains){
-          id = contains[i];
-          exercises[id] = true;
-      }
-      var unit = find_useful_content(exercises,user_id,not_in);*/
       var goal = {}; goal[node_id] = true;
       var unit = find_useful_content(goal,user_id,not_in);
       Meteor.users.update({_id:user_id},{$set:{goal:node_id,nextUnit:unit}});
