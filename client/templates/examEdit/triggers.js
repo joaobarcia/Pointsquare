30d86e0d335f046aa48c04ecd135101dc5ffffdc
joaobarcia <joaobarcia@gmail.com>
2016-09-13 17:59:10 +0100

    million exam fixes

--- client/templates/examEdit/triggers.js
+++ client/templates/examEdit/triggers.js
@@ -1,9 +1,9 @@
 Template.examEdit.onCreated(function() {
-    //console.log('conceptEdit >onCreated');
-    var self = this;
-    self.autorun(function() {
-        var examId = FlowRouter.getParam('nodeId');
-    });
+  //console.log('conceptEdit >onCreated');
+  var self = this;
+  self.autorun(function() {
+    var examId = FlowRouter.getParam('nodeId');
+  });
 });
 
 Template.examEdit.events({
@@ -13,4 +13,32 @@ Template.examEdit.events({
     Meteor.call('removeExam', nodeId);
     FlowRouter.go('dashboard');
   },
+});
+
+AutoForm.hooks({
+  examEdit: {
+    onSubmit: function(doc) {
+      var nodeId = FlowRouter.getParam('nodeId');
+
+      var examDefinitions = {}
+      examDefinitions = doc;
+      var examContains = doc.exercises; //exercisesMappedAsObject;
+      delete doc.exercises
+
+      console.log(examDefinitions);
+
+      Meteor.call('editNode', nodeId, examDefinitions, function(error, result) {
+        Meteor.call('editContains', nodeId, examContains, function(error, result) {
+          FlowRouter.go('/exam/' + nodeId);
+        });
+
+        //FlowRouter.go('search');
+      });
+      //Meteor.call('editContains', nodeId, examContains);
+
+      //FlowRouter.go('/exam/' + nodeId);
+      this.done();
+      return false;
+    }
+  }
 });
