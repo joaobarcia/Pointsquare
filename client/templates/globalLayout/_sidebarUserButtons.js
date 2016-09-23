f9f3a08eefc47ddb19047c90f370e39f6ef5bf57
joaobarcia <joaobarcia@gmail.com>
2016-09-23 13:46:51 +0200

    meh...

--- client/templates/globalLayout/_sidebarUserButtons.js
+++ client/templates/globalLayout/_sidebarUserButtons.js
@@ -1,13 +1,13 @@
-Template._sidebarUserButtons.rendered = function() {
-    $(document).ready(function() {
-        $('.modal-trigger').leanModal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 300, // Transition in duration
-            out_duration: 200, // Transition out duration
-        });
-        $('.tooltipped').tooltip({
-            delay: 20
-        });
-    });
-}
+// Template._sidebarUserButtons.rendered = function() {
+//     $(document).ready(function() {
+//         $('.modal-trigger').leanModal({
+//             dismissible: true, // Modal can be dismissed by clicking outside of the modal
+//             opacity: .5, // Opacity of modal background
+//             in_duration: 300, // Transition in duration
+//             out_duration: 200, // Transition out duration
+//         });
+//         $('.tooltipped').tooltip({
+//             delay: 20
+//         });
+//     });
+// }
