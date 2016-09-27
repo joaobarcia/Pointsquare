4a2f6f211f5065d17d1b0b90c17a41a842ea51c3
joaobarcia <joaobarcia@gmail.com>
2016-09-27 23:17:04 +0200

    same

--- client/templates/contentByType/triggers.js
+++ client/templates/contentByType/triggers.js
@@ -16,11 +16,11 @@ Template.contentByType.events({
   'click .next-tab': function(event, template) {
     event.preventDefault();
     const currentTab = parseInt($(event.target).attr('current-tab'));
-    console.log(currentTab);
+    // console.log(currentTab);
     const desiredTab = currentTab + 1;
-    console.log(desiredTab);
+    // console.log(desiredTab);
     const desiredTabSelector = $('.unit-tabs .item[data-tab=' + desiredTab + ']');
-    console.log(desiredTabSelector);
+    // console.log(desiredTabSelector);
 
     $.tab('change tab', desiredTab);
 
