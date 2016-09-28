2cebd0f5b84bf941b6108bcd8aa6f7b65b344617
joaobarcia <joaobarcia@gmail.com>
2016-09-28 23:08:20 +0200

    examPage progress

--- client/templates/examPage/helpers.js
+++ client/templates/examPage/helpers.js
@@ -31,12 +31,10 @@ Template.examInfoBar.helpers({
     const nodeId = this._id;
     if (typeof Meteor.globalFunctions.getNeeds(nodeId) !== 'undefined' && Meteor.globalFunctions.getNeeds(nodeId) !== null) {
       const languagesIds = Object.keys(Meteor.globalFunctions.getNeeds(nodeId).languages);
-      // console.log(languagesIds);
       var examLanguagesNames = [];
       for (let id of languagesIds){
         examLanguagesNames.push(Nodes.findOne(id).name);
       }
-      console.log(examLanguagesNames);
       return examLanguagesNames
     }
   }
