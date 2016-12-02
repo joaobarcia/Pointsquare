d598f72cbde6820b1ea0e274504915f8c5b33960
joaobarcia <joaobarcia@gmail.com>
2016-12-02 17:41:00 +0100

    feedback triggers

--- client/templates/feedbackModal/triggers.js
+++ client/templates/feedbackModal/triggers.js
@@ -4,12 +4,16 @@ Template.feedbackModalContent.events({
     const sender = $('#feedbackEmail')[0].value;
     const msg = $('#feedbackMessage')[0].value;
     const availableForContact = $('#feedbackContact')[0].value;
-    const emailContents = "Sender: " + sender + "<br>" + "Message: " + msg + "<br>" + "Available for contact: " + availableForContact;
+    const emailContents = "Sender: " + sender + " | " + "Message: " + msg + " | " + "Available for contact: " + availableForContact;
     Meteor.call('sendFeedbackEmail',
       'joaobarcia@gmail.com', 'contact@pointsquare.org',
       'Pointsquare - Website Feedback Email', emailContents);
 
     toastr.info('Thanks! :)');
+
+    $('#feedbackEmail')[0].value = "";
+    $('#feedbackMessage')[0].value = "";
+    $('#feedbackContact')[0].value = "";
   }
 });
 
